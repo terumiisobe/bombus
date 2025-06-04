dd18afcd70609fbda5568e442cc4d889cdadc827
terumiisobe <isobe.terumi@hotmail.com>
2025-06-04 19:31:21 -0300

    add conversion funcs to enums

--- domain/species.go
+++ domain/species.go
@@ -10,3 +10,22 @@ const (
 	ScaptotrigonaBipunctata
 	ScaptotrigonaDepilis
 )
+
+func (s Species) String() string {
+	switch s {
+	case TetragosniscaAngustula:
+		return "Tetragosnisca Angustula"
+	case PlebeiaSp:
+		return "Plebeia Sp"
+	case MeliponaQuadrifasciata:
+		return "Melipona Quadrifasciata"
+	case ScaptotrigonaBipunctata:
+		return "Scaptotrigona Bipunctata"
+	case ScaptotrigonaDepilis:
+		return "Scaptotrigona Depilis"
+	case MeliponaBicolor:
+		return "Melipona Bicolor"
+	default:
+		return "Espécie Desconhecida"
+	}
+}
--- domain/status.go
+++ domain/status.go
@@ -9,3 +9,20 @@ const (
 	PetBottle
 	Empty
 )
+
+func (s Status) String() string {
+	switch s {
+	case HoneyReady:
+		return "com mel"
+	case Induzida:
+		return "induzida"
+	case Developing:
+		return "em desenvolvimento"
+	case PetBottle:
+		return "PET"
+	case Empty:
+		return "vazia"
+	default:
+		return "desconhecido"
+	}
+}
