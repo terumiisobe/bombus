00e645b4e2c9304093d24bf6cc27783f7db1e1a8
terumiisobe <isobe.terumi@hotmail.com>
2025-06-06 23:50:08 -0300

    implement conversion for enum

--- domain/interaction.go
+++ domain/interaction.go
@@ -25,3 +25,24 @@ const (
 	Success
 	Fail
 )
+
+func (i InteractionType) String() string {
+	switch i {
+	case Init:
+		return "Init"
+	case MainMenu:
+		return "MainMenu"
+	case ListColmeias:
+		return "ListColmeias"
+	case AddColmeiaForm:
+		return "AddColmeiaForm"
+	case AddBatchColmeiaForm:
+		return "AddBatchColmeiaForm"
+	case Success:
+		return "Success"
+	case Fail:
+		return "Fail"
+	default:
+		return "Unknown"
+	}
+}
