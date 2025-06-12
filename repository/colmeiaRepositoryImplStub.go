177180768517735b151c4d0cbe5f636438c38d4e
terumiisobe <isobe.terumi@hotmail.com>
2025-06-12 14:31:10 -0300

    revert pointer in repo stub

--- repository/colmeiaRepositoryImplStub.go
+++ repository/colmeiaRepositoryImplStub.go
@@ -11,7 +11,7 @@ type ColmeiaRepositoryImplStub struct {
 	colmeias []domain.Colmeia
 }
 
-func NewColmeiaRepositoryImplStub() *ColmeiaRepositoryImplStub {
+func NewColmeiaRepositoryImplStub() ColmeiaRepositoryImplStub {
 	mockTime := time.Date(2025, time.April, 15, 10, 30, 0, 0, time.UTC)
 	colmeias := []domain.Colmeia{
 		{int(123), intPtr(123), nil, domain.TetragosniscaAngustula, mockTime, domain.Developing},
@@ -19,22 +19,22 @@ func NewColmeiaRepositoryImplStub() *ColmeiaRepositoryImplStub {
 		{int(789), intPtr(789), nil, domain.MeliponaQuadrifasciata, mockTime, domain.Developing},
 	}
 
-	return &ColmeiaRepositoryImplStub{colmeias}
+	return ColmeiaRepositoryImplStub{colmeias}
 }
 
-func NewColmeiaRepositoryImplStubCustomData(colmeias []domain.Colmeia) *ColmeiaRepositoryImplStub {
-	return &ColmeiaRepositoryImplStub{colmeias}
+func NewColmeiaRepositoryImplStubCustomData(colmeias []domain.Colmeia) ColmeiaRepositoryImplStub {
+	return ColmeiaRepositoryImplStub{colmeias}
 }
 
 func intPtr(i int) *int {
 	return &i
 }
 
-func (s *ColmeiaRepositoryImplStub) FindAll(species, status string) ([]domain.Colmeia, *errs.AppError) {
+func (s ColmeiaRepositoryImplStub) FindAll(species, status string) ([]domain.Colmeia, *errs.AppError) {
 	return s.colmeias, nil
 }
 
-func (s *ColmeiaRepositoryImplStub) ById(id string) (domain.Colmeia, *errs.AppError) {
+func (s ColmeiaRepositoryImplStub) ById(id string) (domain.Colmeia, *errs.AppError) {
 	var colmeia domain.Colmeia
 	colmeiaID, _ := strconv.Atoi(id)
 	for _, colmeia := range s.colmeias {
@@ -45,12 +45,12 @@ func (s *ColmeiaRepositoryImplStub) ById(id string) (domain.Colmeia, *errs.AppEr
 	return colmeia, errs.NewNotFoundError("Colmeia not found")
 }
 
-func (s *ColmeiaRepositoryImplStub) Create(colmeia domain.Colmeia) *errs.AppError {
+func (s ColmeiaRepositoryImplStub) Create(colmeia domain.Colmeia) *errs.AppError {
 	s.colmeias = append(s.colmeias, colmeia)
 	return nil
 }
 
-func (s *ColmeiaRepositoryImplStub) CountBySpecies() (map[string]int, *errs.AppError) {
+func (s ColmeiaRepositoryImplStub) CountBySpecies() (map[string]int, *errs.AppError) {
 	count := make(map[string]int)
 	for _, colmeia := range s.colmeias {
 		key := strconv.Itoa(int(colmeia.Species))
@@ -59,7 +59,7 @@ func (s *ColmeiaRepositoryImplStub) CountBySpecies() (map[string]int, *errs.AppE
 	return count, nil
 }
 
-func (s *ColmeiaRepositoryImplStub) CountBySpeciesAndStatus() (map[string]map[string]int, *errs.AppError) {
+func (s ColmeiaRepositoryImplStub) CountBySpeciesAndStatus() (map[string]map[string]int, *errs.AppError) {
 	count := make(map[string]map[string]int)
 	for _, colmeia := range s.colmeias {
 		species := strconv.Itoa(int(colmeia.Species))
@@ -73,6 +73,6 @@ func (s *ColmeiaRepositoryImplStub) CountBySpeciesAndStatus() (map[string]map[st
 	return count, nil
 }
 
-func (s *ColmeiaRepositoryImplStub) GetColmeias() []domain.Colmeia {
+func (s ColmeiaRepositoryImplStub) GetColmeias() []domain.Colmeia {
 	return s.colmeias
 }
