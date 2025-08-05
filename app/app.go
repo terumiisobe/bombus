af36e0c8641861dc82a875a7e6cd6bf82ee3551d
terumiisobe <isobe.terumi@hotmail.com>
2025-08-05 16:48:32 -0300

    add cors middleware for localhost access

--- app/app.go
+++ app/app.go
@@ -55,9 +55,24 @@ func Start() {
 	if port == "" {
 		port = "8080"
 	}
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	log.Fatal(http.ListenAndServe(":"+port, corsMiddleware(router)))
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello")
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
